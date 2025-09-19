b6d07c61e694e1171a6fdd04c4ab9499b50eecec
Aditya Raj <adityakarn06@gmail.com>
2025-09-20 03:24:39 +0530

    add-socket-connection

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,4 +1,17 @@
+import { useState } from "react"
+import { Link } from "react-router-dom"
 
 export const LandingPage = () => {
-    return <div>Landing Page</div>
+    const [name, setName] = useState("")
+
+    return (
+        <div>
+            <input type="text" onChange={(e) => {
+                setName(e.target.value)
+            }} />
+            <Link to={`/room?name=${name}`}>
+                Join
+            </Link>
+        </div>
+    )
 }
--- src/components/Room.tsx
+++ src/components/Room.tsx
@@ -1,4 +1,65 @@
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom"
+import type { Socket } from "socket.io-client";
+import { io } from "socket.io-client";
+
+const URL = "http://localhost:3000"
 
 export const Room = () => {
-    return <div>Room</div>
+    const [searchParams, setSearchParams] = useSearchParams();
+    const name = searchParams.get("name");
+    const [socket, setSocket] = useState<Socket | null>(null);
+    const [lobby, setLobby] = useState(true);
+
+    useEffect(() => {
+        if (!name) {
+            window.location.href = "/"
+        }
+        
+        const socket = io(URL);
+        
+        socket.on("send-offer", ({roomId}) => {
+            setLobby(false);
+            socket.emit("offer", {
+                sdp: "",
+                roomId,
+            })
+        })
+
+        socket.on("offer", ({ roomId, offer }) => {
+            setLobby(false);
+            socket.emit("answer", {
+                sdp: "",
+                roomId,
+            })
+        })
+
+        socket.on("answer", ({ roomId, answer }) => {
+            setLobby(false);
+        })
+
+        socket.on("lobby", () => {
+            setLobby(true);
+        })
+        
+        setSocket(socket);
+
+        return () => {
+            socket.disconnect();
+        }
+    }, [name])
+
+    if (lobby) {
+        return (<>
+            In Lobby... Waiting for other user to join
+        </>)
+    }
+
+    return (
+        <>
+            Hi {name}
+            <video width={400} height={400} />
+            <video width={400} height={400} />
+        </>
+    )
 }
